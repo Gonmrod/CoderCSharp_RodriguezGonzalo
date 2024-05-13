e9c4bf4e334bf5ddbb22c34407253dba39680009
Gonzalo Rodriguez <gonzalo.marodriguez@gmail.com>
2024-05-13 17:32:12 -0300

    Agregar archivos de proyecto.

---
+++ PreEntrega1_RodriguezGonzalo/Clases/Modelos/Producto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Modelos
+{
+    public class Producto
+    {
+        #region Propiedades
+
+        protected int Id { get; set; }
+        protected string Descripcion { get; set; }
+        protected double Costo { get; set; }
+        protected double PrecioVenta { get; set; }
+        protected int Stock { get; set; }
+        protected int IdUsuario { get; set; }
+
+        #endregion
+
+        #region Contructor
+
+        public Producto(int id, string descripcion, double costo, double precioVenta, int stock, int idUsuario)
+        {
+            this.Id = id;
+            this.Descripcion = descripcion;
+            this.Costo = costo;
+            this.PrecioVenta = precioVenta;
+            this.Stock = stock;
+            this.IdUsuario = idUsuario;
+        }
+
+        #endregion
+    }
+}
---
+++ PreEntrega1_RodriguezGonzalo/Clases/Modelos/Usuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases.Modelos
+{
+
+    public class Usuario
+    {
+
+    #region Propiedades
+        //Se crean las propiedades con modificador protected, dado que interpreto que tendremos varios tipos de usuarios, y así podremos realizar la correspondiente herencia.
+
+        protected int Id {  get; set; }
+        protected string Nombre { get; set; }
+        protected string Apellido { get; set; }
+        protected string NombreUsuario { get; set; }
+        protected string Contrasenia { get; set; }
+        protected string Mail { get; set; }
+
+        #endregion
+
+    #region Constructor
+    //Constructor
+
+    public Usuario(int id, string nombre, string apellido, string nombreUsuario, string contrasenia, string Mail )
+    {
+        this.Id = id;
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.NombreUsuario = nombreUsuario;
+        this.Contrasenia = contrasenia;
+        this.Mail = Mail;
+    }
+    #endregion
+
+    }
+}
