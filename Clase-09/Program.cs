8cc2b0a26670b2b6c04e7ddf2d32be0c03300e37
Gonzalo Rodriguez <gonzalo.marodriguez@gmail.com>
2024-05-22 19:44:01 -0300

    Clase09-AdoConectado

---
+++ Clase-09/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+
+namespace Clase09
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=localhost\SQLEXPRESS; Database=SistemaGestion ; Trusted_Connection=True; User=sa; Password=Admin1234;";
+
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Usuario";
+
+                using (SqlCommand comando = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader dataReader = comando.ExecuteReader())
+                    {
+                        if (dataReader.HasRows)
+                        {
+                            while (dataReader.Read())
+                            {
+                                var dato = dataReader.GetString(1);
+                                Console.WriteLine(dato);
+                            }
+                        }
+                    }
+                connection.Close();
+                }
+
+            }
+        } 
+    }
+}
